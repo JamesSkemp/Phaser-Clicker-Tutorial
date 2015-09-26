1780f82a4a50ffdd107d0c6be7bbaba60093359d
James Skemp <strivinglife@gmail.com>
2015-09-26 07:22:31 -0500

    Now displaying a background image in the game.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Phaser application based upon [Ben Sparks' tutorial](http://gamedevacademy.org/p
 ## Assets
 Assets in assets/allacrost_enemy_sprites are licensed under a CC-BY-SA 3.0 license, and are available from http://opengameart.org/content/2d-rpg-enemy-set
 > Various people produced the sprites in this set. Please attribute to either "Hero of Allacrost - http://www.allacrost.org" or the individual artists listed below. - Brett Steele (Safir-Kreuz) - Joe Raucci (Sylon) - Vicki Beinhart (Namakoro) - Tyler Olsen (Roots)
+
+Assets in assets/parallax_forest_pack are licensed under a CC0 license, and are available from http://opengameart.org/content/forest-background
+> Author is [ansimuz](http://opengameart.org/users/ansimuz) on OpenGameArt.org
--- src/game.js
+++ src/game.js
@@ -4,10 +4,27 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO);
 
 game.state.add('play', {
 	preload: function () {
+		// Load in the images for our background.
+		game.load.image('forest-back', 'assets/parallax_forest_pack/layers/parallax-forest-back-trees.png');
+		game.load.image('forest-lights', 'assets/parallax_forest_pack/layers/parallax-forest-lights.png');
+		game.load.image('forest-middle', 'assets/parallax_forest_pack/layers/parallax-forest-middle-trees.png');
+		game.load.image('forest-front', 'assets/parallax_forest_pack/layers/parallax-forest-front-trees.png');
+
 		// Sizes are the width and height of an individual image, and how many frames there are.
 		game.load.spritesheet('skeleton', 'assets/allacrost_enemy_sprites/skeleton.png', 256 / 4, 128, 4);
 	},
 	create: function () {
+		var state = this;
+		// Create a group to hold our related background images.
+		this.background = this.game.add.group();
+		// Setup each of our background layers to take the full screen.
+		['forest-back', 'forest-lights', 'forest-middle', 'forest-front']
+			.forEach(function (image) {
+				var bg = state.game.add.tileSprite(0, 0, state.game.world.width, state.game.world.height, image, '', state.background);
+				// If not set, the image will repeat horizontally and vertically (4x4).
+				bg.tileScale.setTo(4, 4);
+			});
+
 		// Location of the image, and in this case the frame to use (zero-based as usual).
 		var skeletonSprite = game.add.sprite(450, 290, 'skeleton', 0);
 		// Set the rotation point to the center of the image, instead of the top left (0, 0).
