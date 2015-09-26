d991da415e66f622ddd2831e1b304f2324654e18
James Skemp <strivinglife@gmail.com>
2015-09-26 06:33:30 -0500

    Added empty states we'll use.

--- src/game.js
+++ src/game.js
@@ -1,2 +1,17 @@
 ﻿/// <reference path="../lib/phaser-2.4.3.js" />
+// Create a game with dimensions 800x600, and use the WebGL (default) or Canvas renderer, depending upon the browser.
 var game = new Phaser.Game(800, 600, Phaser.AUTO);
+
+game.state.add('play', {
+	preload: function () {
+
+	},
+	create: function () {
+
+	},
+	render: function () {
+
+	}
+});
+
+game.state.start('play');
