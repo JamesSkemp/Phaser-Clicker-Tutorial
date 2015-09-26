cb7b3cf712483051db5c0b625caf265a050d965b
James Skemp <strivinglife@gmail.com>
2015-09-26 06:43:09 -0500

    Added skeleton and text to display. Didn't cut out the first frame, so mine isn't correct, yet.

--- src/game.js
+++ src/game.js
@@ -4,13 +4,14 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO);
 
 game.state.add('play', {
 	preload: function () {
-
+		game.load.image('skeleton', 'assets/allacrost_enemy_sprites/skeleton.png');
 	},
 	create: function () {
-
+		var skeletonSprite = game.add.sprite(450, 290, 'skeleton');
+		skeletonSprite.anchor.setTo(0.5, 0.5);
 	},
 	render: function () {
-
+		game.debug.text('Adventure Awaits!', 250, 250);
 	}
 });
 
