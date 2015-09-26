96091645778c032f1f06be5061b11167053ee5f1
James Skemp <strivinglife@gmail.com>
2015-09-26 06:54:50 -0500

    Skeleton now displays correctly.

--- src/game.js
+++ src/game.js
@@ -4,10 +4,13 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO);
 
 game.state.add('play', {
 	preload: function () {
-		game.load.image('skeleton', 'assets/allacrost_enemy_sprites/skeleton.png');
+		// Sizes are the width and height of an individual image, and how many frames there are.
+		game.load.spritesheet('skeleton', 'assets/allacrost_enemy_sprites/skeleton.png', 256 / 4, 128, 4);
 	},
 	create: function () {
-		var skeletonSprite = game.add.sprite(450, 290, 'skeleton');
+		// Location of the image, and in this case the frame to use (zero-based as usual).
+		var skeletonSprite = game.add.sprite(450, 290, 'skeleton', 0);
+		// Set the rotation point to the center of the image, instead of the top left (0, 0).
 		skeletonSprite.anchor.setTo(0.5, 0.5);
 	},
 	render: function () {
